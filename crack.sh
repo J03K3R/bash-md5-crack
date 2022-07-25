5bb5983a4ce907e388d580a89acac3e95c3fad68
HaXeZ <joe@joeszone.co.uk>
2022-07-25 11:17:43 +0100

    indents
    added some indents

--- crack.sh
+++ crack.sh
@@ -2,12 +2,12 @@
 VAR2="13c710f7d9d2f13543172f90750c415f  -"
 cat wordlist.txt | while read line 
 do
-   	echo "Trying $line"
-   	VAR1="$(echo -n "$line" | md5sum)"
-	if [ "$VAR1" = "$VAR2" ]; then
-    	echo $i " IS THE CORRECT PASSWORD ✓✓✓"
-	break
+   echo "Trying $line"
+   VAR1="$(echo -n "$line" | md5sum)"
+   	if [ "$VAR1" = "$VAR2" ]; then
+    		echo $i " IS THE CORRECT PASSWORD ✓✓✓"
+		break
 	else
-    	echo $i " INCORRECT PASSWORD ✕✕✕"
+    		echo $i " INCORRECT PASSWORD ✕✕✕"
 	fi
 done
